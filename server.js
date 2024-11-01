57cfb4030a4b6ad186d8e9dd09098e2661b9ad18
Javan <86697319+Dijaa@users.noreply.github.com>
2024-11-01 11:05:48 -0300

    Create server-side logic for handling player connections and room creation
    * **server.js**
      - Create a new file to handle server-side logic using Node.js and Express
      - Import necessary modules: `express`, `http`, and `socket.io`
      - Set up an Express app and HTTP server
      - Initialize Socket.io and handle player connections and room creation
      - Serve the `index.html` file when the root URL is accessed
    
    * **package.json**
      - Add dependencies for `express` and `socket.io`
      - Add start script to run the server
    
    * **package-lock.json**
      - Add lock file for package dependencies

---
+++ server.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+const rooms = {};
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+  console.log('A user connected');
+
+  socket.on('createRoom', (roomId) => {
+    if (!rooms[roomId]) {
+      rooms[roomId] = [];
+    }
+    if (rooms[roomId].length < 2) {
+      rooms[roomId].push(socket.id);
+      socket.join(roomId);
+      socket.emit('roomJoined', roomId);
+      console.log(`User joined room ${roomId}`);
+    } else {
+      socket.emit('roomFull', roomId);
+    }
+  });
+
+  socket.on('joinRoom', (roomId) => {
+    if (rooms[roomId] && rooms[roomId].length < 2) {
+      rooms[roomId].push(socket.id);
+      socket.join(roomId);
+      socket.emit('roomJoined', roomId);
+      console.log(`User joined room ${roomId}`);
+    } else {
+      socket.emit('roomFull', roomId);
+    }
+  });
+
+  socket.on('playerMove', (data) => {
+    const { roomId, celula, botao, playerAtual } = data;
+    socket.to(roomId).emit('playerMove', { celula, botao, playerAtual });
+  });
+
+  socket.on('disconnect', () => {
+    console.log('A user disconnected');
+    for (const roomId in rooms) {
+      rooms[roomId] = rooms[roomId].filter((id) => id !== socket.id);
+      if (rooms[roomId].length === 0) {
+        delete rooms[roomId];
+      }
+    }
+  });
+});
+
+server.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
